6f02305982a757278d763fbd0d7b37755de194a5
pogo123456 <140170136+pogo123456@users.noreply.github.com>
2023-09-13 20:20:13 +0300

    меню, характеристики ТРУБЫ и КС

--- Grigorova_AS4_LR/Grigorova.cpp
+++ Grigorova_AS4_LR/Grigorova.cpp
@@ -1,72 +1,170 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+//Сведения о ТРУБЕ
 struct pipe
 {
 	double len; //km
 	double diam; //mm
 	string km_mark;
-	bool repair;
+	bool inrepair;
+
+	void InputPipe()
+	{
+		cout << "Please, enter pipe length(km)" << endl;
+		cin >> len;
+		cout << "Please, enter pipe diameter(mm)" << endl;
+		cin >> diam;
+		cout << "Please, enter kilometer mark" << endl;
+		cin >> km_mark;
+		cout << "Please, enter pipe inrepair? (1-yes, 0-no)" << endl;
+		cin >> inrepair;
+	}
+
+	void OutputPipe()
+	{
+		cout << "Pipe length: " << len << endl;
+		cout << "Pipe diameter: " << diam << endl;
+		cout << "Kilometer mark: " << km_mark << endl;
+		cout << "Pipe condition: " << inrepair << endl;
+	}
+	
+	void RepairStatus()
+	{
+		inrepair = !inrepair;
+	}
 };
 
+//Сведения о КС
 struct station
 {
 	string title;
 	int workshop;
 	int active_workshop;
 	double efficiency; // о чем вообще речь?? 
+
+	void InputStation()
+	{
+		cout << "Please, enter station name" << endl;
+		cin >> title;
+		cout << "Please, enter number of workshops" << endl;
+		cin >> workshop;
+		cout << "Please, enter number of active workshops" << endl;
+		cin >> active_workshop;
+		cout << "Please, enter effectiveness" << endl;
+		cin >> efficiency;
+	}
+
+	void OutputStation()
+	{
+		cout << "Station name: " << title << endl;
+		cout << "Number of workshops: " << workshop << endl;
+		cout << "Number of active workshops: " << active_workshop << endl;
+		cout << "Effectiveness: " << efficiency << endl;
+	}
+
+	void StartWorkshop()
+	{
+		if (active_workshop < workshop)
+		{
+			active_workshop++;
+		}
+		else
+		{
+			cout << "Everyone is working" << endl;
+		}
+	}
+
+	void StopWorkshop()
+	{
+		if (active_workshop > 0)
+		{
+			active_workshop--;
+		}
+		else
+		{
+			cout << "No one is working" << endl;
+		}
+	}
 };
-
-void InputPipe()
-{
-	pipe pipe;
-	cout << "Please, enter pipe length(km)" << endl;
-	cin >> pipe.len;
-	cout << "Please, enter pipe diameter(mm)" << endl;
-	cin >> pipe.diam;
-	cout << "Please, enter kilometer mark" << endl;
-	cin >> pipe.km_mark;
-	cout << "Please, enter pipe condition" << endl;
-	cin >> pipe.repair;
-}
-
-void InputStation()
-{
-	station station;
-	cout << "Please, enter station name" << endl;
-	cin >> station.title;
-	cout << "Please, enter number of workshops" << endl;
-	cin >> station.workshop;
-	cout << "Please, enter number of active workshops" << endl;
-	cin >> station.active_workshop;
-	cout << "Please, enter effectiveness" << endl;
-	cin >> station.efficiency;
-}
-
-void OutputPipe()
-{
-	pipe pipe;
-	cout << "Pipe length: " << pipe.len << endl;
-	cout << "Pipe diameter: " << pipe.diam << endl;
-	cout << "Kilometer mark: " << pipe.km_mark << endl;
-	cout << "Pipe condition: " << pipe.repair << endl;
-}
-
-void OutputStation()
-{
-	station station;
-	cout << "Station name: " << station.title << endl;
-	cout << "Number of workshops: " << station.workshop << endl;
-	cout << "Number of active workshops: " << station.active_workshop << endl;
-	cout << "Effectiveness: " << station.efficiency << endl;
-}
 
 int main()
 {
-	InputPipe();
-	InputStation();
-	OutputPipe();
-	OutputStation();
+	vector <pipe> pipes;
+	vector <station> stations;
+	
+	while (true)
+	{
+		cout << "Menu: " << endl;
+		cout << "1. Add pipe" << endl;
+		cout << "2. Add station" << endl;
+		cout << "3. All object" << endl;
+		cout << "4. Edit pipe" << endl;
+		cout << "5. Edit station" << endl;
+		cout << "6. Save" << endl;
+		cout << "7. Download" << endl;
+		cout << "8. Exit" << endl;
+
+		int choice;
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 1:
+		{
+			pipe pipe;
+			pipe.InputPipe();
+			pipes.push_back(pipe);
+			break;
+		}
+		case 2:
+		{
+			station station;
+			station.InputStation();
+			stations.push_back(station);
+			break;
+		}
+		/*case 3:
+		{
+			cout << "List of pipes: " << endl;
+			for (const pipe& pipe : pipes)
+			{
+				pipe.OutputPipe();
+				cout << endl;
+			}
+			for (const station & station : stations)
+			{
+				station.OutputStation();
+				cout << endl;
+			}
+			break;
+		}*/
+		case 4:
+		{
+			break;
+		}
+		case 5:
+		{
+			break;
+		}
+		case 6:
+		{
+			break;
+		}
+		case 7:
+		{
+			break;
+		}
+		case 0:
+			return 0;
+		default:
+			cout << "Error!!! Try again." << endl;
+			break;
+		}
+	}
+	
 	return 0;
 }
